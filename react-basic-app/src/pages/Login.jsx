86b60202d7f865456759da24f9d23d8fea547e59
Gowsika V <gowsikavenkatachslam0713@gmail.com>
2025-11-17 15:22:51 +0530

    Add index page with Navbar, Footer, and routing setup

---
+++ react-basic-app/src/pages/Login.jsx
@@ -0,0 +1,10 @@
+function Login() {
+  return (
+    <div className="container mt-4">
+      <h1>Login Page</h1>
+      <p>Here we will add login form later.</p>
+    </div>
+  );
+}
+
+export default Login;
