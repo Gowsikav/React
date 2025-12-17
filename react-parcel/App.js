6256c05caaa21aabc1e54907e8d827a9f09e1fcc
Gowsika V <gowsikavenkatachslam0713@gmail.com>
2025-12-17 13:11:47 +0530

    created two pratice react project

---
+++ react-parcel/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+let heading = React.createElement("div", {id:"seconddiv"}, [
+        React.createElement("h1", {}, "Hello World from React!"),
+        React.createElement("h2", {}, "This is a subheading"),
+        React.createElement("p", {}, "This is a paragraph created using React."),
+        React.createElement("div",{ id:"innerdiv"},[
+            React.createElement("a", { href:"https://x-workz.com/"}, "Xworkz Link",),
+            React.createElement('img',{src:"https://raw.githubusercontent.com/x-workzdev/Xworkz-images/develop/Gallery/45.jpeg", alt:"Xworkz Logo",width:'150px',height:'150px'})
+        ])
+      ]);
+
+let root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
