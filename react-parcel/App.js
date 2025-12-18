f3d7555903290b2087468ca126a0d5d8996924e0
Gowsika V <gowsikavenkatachslam0713@gmail.com>
2025-12-18 12:35:28 +0530

    used jsx basic syntax insted of complex code

--- react-parcel/App.js
+++ react-parcel/App.js
@@ -1,15 +1,38 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-let heading = React.createElement("div", {id:"seconddiv"}, [
-        React.createElement("h1", {}, "Hello World from React!"),
-        React.createElement("h2", {}, "This is a subheading"),
-        React.createElement("p", {}, "This is a paragraph created using React."),
-        React.createElement("div",{ id:"innerdiv"},[
-            React.createElement("a", { href:"https://x-workz.com/"}, "Xworkz Link",),
-            React.createElement('img',{src:"https://raw.githubusercontent.com/x-workzdev/Xworkz-images/develop/Gallery/45.jpeg", alt:"Xworkz Logo",width:'150px',height:'150px'})
-        ])
-      ]);
+let heading = React.createElement("div", { id: "seconddiv" }, [
+  React.createElement("h1", {}, "Hello World from React!"),
+  React.createElement("h2", {}, "This is a subheading"),
+  React.createElement("p", {}, "This is a paragraph created using React."),
+  React.createElement("div", { id: "innerdiv" }, [
+    React.createElement("a", { href: "https://x-workz.com/" }, "Xworkz Link"),
+    React.createElement("img", {
+      src: "https://raw.githubusercontent.com/x-workzdev/Xworkz-images/develop/Gallery/45.jpeg",
+      alt: "Xworkz Logo",
+      width: "150px",
+      height: "150px",
+    }),
+  ]),
+]);
+
+let head = (
+  <div id="seconddiv">
+    <h1>Hello World from react</h1>
+    <h2>This is a sub heading</h2>
+    <p>This is a paragraph created using React</p>
+    <div id="inndediv">
+      <a href="https://x-workz.com/">Xworkz Link</a>
+      <img
+        src="https://raw.githubusercontent.com/x-workzdev/Xworkz-images/develop/Gallery/45.jpeg"
+        alt="Xworkz Logo"
+        width="150 px"
+        height="150px"
+      />
+    </div>
+  </div>
+);
 
 let root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading);
+root.render(head);
