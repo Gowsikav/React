e4a112651e278d580ac070036011fd273f67d5e5
Gowsika V <gowsikavenkatachslam0713@gmail.com>
2025-12-31 13:02:50 +0530

    todo application created inside counter app

--- coding-practice/src/components/Footer.jsx
+++ coding-practice/src/components/Footer.jsx
@@ -1,7 +1,7 @@
 export function Footer() {
-    return (
-        <div className="footer">
-            <h3>© 2025 Counter App | Built with React</h3>
-        </div>
-    );
+  return (
+    <div className="footer">
+      <h3>© 2025 Counter & Todo App | Built with React</h3>
+    </div>
+  );
 }
---
+++ coding-practice/src/components/Todo.jsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+
+export function Todo() {
+  const [task, setTask] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = () => {
+    if (task.trim() === "") return;
+    setTodos([ task,...todos]);
+    setTask("");
+  };
+
+  const deleteTodo = (index) => {
+    setTodos(todos.filter((_, i) => i !== index));
+  };
+
+  return (
+    <div className="todo">
+      <h2>Todo List</h2>
+
+      <input
+        type="text"
+        placeholder="Enter task"
+        value={task}
+        onChange={(e) => setTask(e.target.value)}
+      />
+      <button onClick={addTodo}>Add</button>
+
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={index}>
+            {todo}
+            <button onClick={() => deleteTodo(index)}> ❌</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
