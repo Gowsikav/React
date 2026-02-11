5a484dc6808184287895f39fbb39c01eef00dd89
Gowsika V <gowsikavenkatachslam0713@gmail.com>
2026-02-11 20:00:00 +0530

    created two class based component and used in About component

--- react-parcel/src/components/About.jsx
+++ react-parcel/src/components/About.jsx
@@ -1,8 +1,13 @@
+import AboutClass from "./AboutClass";
+import AboutContent from "./AboutContent";
 
 export let About = ()=>{
     return(
         <div>
             <p>About page</p>
+            <AboutContent/>
+            <hr></hr>
+            <AboutClass/>
         </div>
     );
 }
---
+++ react-parcel/src/components/AboutContent.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+class AboutContent extends React.Component {
+  render() {
+    return (
+      <div>
+        <h4>Class based Component</h4>
+        <h4>X-workz ODC</h4>
+        <h6>Software training organisation</h6>
+      </div>
+    );
+  }
+}
+
+export default AboutContent;
